c8788c9a1e9192634a61d1307b62dd8059d0221a
velsa <velshome@yahoo.com>
2013-02-13 00:35:32 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+yota
+====
+
+CasperJS script for viewing and changing the current network speed of your Yota 4G
+
+### Instructions for Mac:
+
+Install CasperJS using [HomeBrew](http://mxcl.github.com/homebrew/)
+
+    brew install casperjs
+
+Run yota.js script like this
+    
+    casperjs yota.js
+
+You will get usage instructions
+
+### Bash conveniece:
+
+To run it from command line conveniently, add this to your `~/.bashrc`
+
+    function yota() { 
+        casperjs ~/yota.js $@ 
+    }
+
+And then you can simply run it like this:
+
+    ~ > yota status
---
+++ yota.js
@@ -0,0 +1,194 @@
+//
+// SET YOUR CREDENTIALS HERE
+//
+var login       = null,
+    password    = null;
+
+
+
+
+
+//
+// DON'T MODIFY ANYTHING BELOW THIS LINE
+//
+// ----------------------------------------------------------------------------------------------
+//
+var ip_url      = 'http://www.myglobalip.com/',
+    base_url    = 'https://my.yota.ru/selfcare/',
+    login_url   = 'login',
+    dev_url     = 'devices';
+var casper = require('casper').create({
+    verbose: false,
+    logLevel: 'debug'
+});
+
+show_usage_and_exit = function() {
+    casper.echo("Usage: yota [ status | list | set <desired-speed> ]");
+    casper.echo("\tstatus\t- Показывает текущий план и остаток дней");
+    casper.echo("\tlist\t- Показывает список доступных скоростей для команды set");
+    casper.echo("\tset\t- Устанавливает скорость Интернета");
+    casper.exit();
+};
+
+if (!login || !password) {
+    casper.echo("Пожалуйста настройте ваш login и password в файле yota.js !");
+    casper.exit();
+}
+
+if (!casper.cli.args.length) {
+    show_usage_and_exit();
+}
+
+if (casper.cli.args[0] === 'set') {
+    if (casper.cli.args.length < 2) {
+        casper.echo("Пожалуйста, укажите скорость (используйте команду 'list' для списка доступных парамтеров)");
+        show_usage_and_exit();
+    }
+    if (casper.cli.args[1] === 64) {
+        casper.echo("Бесплатный доступ (64 кбит/с) не может быть установлен при активной подписке");
+        casper.exit();
+    }
+}
+
+
+casper.start(ip_url);
+
+// casper.then(function() {
+//      this.capture('0.png', {
+//         top: 100,
+//         left: 0,
+//         width: 600,
+//         height: 600
+//     });
+// });
+
+casper.then(function() {
+    var ip = this.evaluate(function() {
+        var ip = $(".ip").get(0).innerText.split(".");
+        return ip;
+    });
+    if (!ip) {
+        this.echo("Нет подключения к Интернету ?!");
+        this.exit();
+    }
+    if (ip[0] === '178' && ip[1] === '176')
+        this.echo("Вы подключены к Интернету через Yota");
+    else
+        this.echo("ВНИМАНИЕ: Вы НЕ подключены к Интернету через Yota !");
+});
+
+casper.thenOpen(base_url+'login');
+
+// casper.then(function() {
+//     this.test.assertExists('input[name=IDToken1]', 'login field is found');
+//     this.test.assertExists('input[name=IDToken1]', 'password field is found');
+//     this.test.assertExists('#doSubmitLoginForm', 'submit button is found');
+// });
+
+casper.thenEvaluate(function(login, password) {
+    $('input[name=IDToken1]').attr('value', login);
+    $('input[name=IDToken3]').attr('value', password);
+    $('#doSubmitLoginForm').click();
+}, login, password);
+
+// casper.then(function() {
+//      this.capture('1.png', {
+//         top: 100,
+//         left: 0,
+//         width: 600,
+//         height: 600
+//     });
+// });
+
+casper.thenOpen(base_url+'devices');
+
+// casper.then(function() {
+//      this.capture('2.png', {
+//         top: 100,
+//         left: 0,
+//         width: 600,
+//         height: 600
+//     });
+// });
+
+switch (casper.cli.args[0]) {
+    case 'status':
+        casper.then(function() {
+            var current = this.evaluate(function() {
+                var result = '';
+                var f = $('.tariff-choice-form');
+                var steps = sliderData[f.find('input[name="product"]').val()].steps;
+                var offerCode = f.find('input[name="offerCode"]').val();
+                for (var i = 0; i < steps.length; i++) {
+                    if(steps[i].code == offerCode) {
+                        result = 'План: '+steps[i].name + '\nОсталось: ' + steps[i].remainNumber + ' дней';
+                        break;
+                    }
+                }
+                return result;
+            });
+            this.echo(current);
+        });
+    break;
+
+    case 'list':
+        casper.then(function() {
+            var variants = this.evaluate(function() {
+                var variants = [];
+                var f = $('.tariff-choice-form');
+                var steps = sliderData[f.find('input[name="product"]').val()].steps;
+                for (var i = 0; i < steps.length; i++) {
+                    var speedNumber = steps[i].speedNumber;
+                    variants[i] = (/max/.test(speedNumber) ? 'max : ' : speedNumber +' : ') + (steps[i].name || steps[i].description) + '(остаток ' + steps[i].remainNumber + ')';
+                }
+                return variants;
+            });
+            for (var i = 0; i < variants.length; i++) {
+                this.echo(variants[i]);
+            }
+        });
+    break;
+
+    case "set":
+        casper.then(function() {
+            var new_status = this.evaluate(function(speed) {
+                var f = $('.tariff-choice-form');
+                var steps = sliderData[f.find('input[name="product"]').val()].steps;
+                var currentOfferCode = f.find('input[name="offerCode"]').val();
+                var offerCode = null;
+                var status = "";
+                var isDisablingAutoprolong = false;
+                for (var i = 0; i < steps.length; i++) {
+                    if (steps[i].speedNumber == speed || (speed == 'max' && steps[i].speedNumber.contains('max'))) {
+                        offerCode = steps[i].code;
+                        status = 'Новый План: '+steps[i].name + '\nОсталось: ' + steps[i].remainNumber + ' дней';
+                        isDisablingAutoprolong = steps[i].isDisablingAutoprolong;
+                    }
+                }
+                if (offerCode && currentOfferCode != offerCode) {
+                    f.find("form").append("<input type='hidden' name='isDisablingAutoprolong' value='" + isDisablingAutoprolong + "'/>");
+                    f.find('[name="offerCode"]').val(offerCode);
+                    f.find('[name="productOfferingCode"]').val(offerCode);
+                    f.submit();
+                }
+
+                return status;
+            }, casper.cli.args[1]);
+            this.echo(new_status);
+        });
+    break;
+    default:
+        casper.echo("Неизвестный аргумент " + casper.cli.args[0]);
+        show_usage_and_exit();
+}
+
+casper.then(function() {
+     this.capture('3.png', {
+        top: 100,
+        left: 0,
+        width: 600,
+        height: 600
+    });
+});
+
+casper.run();
